d0038badbdfde924f494d5e800c8b42526e3c419
bhseong <bitzflex@gmail.com>
2022-03-07 09:52:59 +0900

    Initial Commit

---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by bhseong on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
